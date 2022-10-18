73ef1dfefbb124365dfdb12dadd90674e4f64a62
Yeezy97 <ala.rah@outlook.com>
2022-10-18 17:58:32 +0100

    small changes

--- lib/constants/top_navigation.dart
+++ lib/constants/top_navigation.dart
@@ -4,9 +4,8 @@ import 'package:flutterwebb/helpers/responsiveness.dart';
 
 AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
     AppBar(
-      automaticallyImplyLeading: false,
       leading: !Responsiveness.isSmallScreen(context) ?
-      SizedBox(child: Image.asset("assets/logo1.png",),) : IconButton(
+      Image.asset("assets/logo.png",) : IconButton(
           icon: const Icon(Icons.menu,color: Colors.black,),
         onPressed: (){
           key.currentState!.openDrawer();
