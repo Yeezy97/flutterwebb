82f3a8ef85d51fbd57913bff9dfbdb3434ea585e
Yeezy97 <ala.rah@outlook.com>
2022-10-21 21:07:42 +0100

    vertical menu items

--- lib/widgets/vertical_menu_items.dart
+++ lib/widgets/vertical_menu_items.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
+import '../constants/controllers.dart';
+
 class VerticalMenuItems extends StatelessWidget {
-  const VerticalMenuItems({Key? key}) : super(key: key);
+  const VerticalMenuItems({Key? key, this.itemName, this.onTap})
+      : super(key: key);
+
+  final String? itemName;
+  final Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return InkWell(
+      onTap: onTap!,
+      onHover: (value) {
+        value
+            ? menuController.onHover(itemName!)
+            : menuController.onHover("not hovering");
+      },
+    );
   }
 }
