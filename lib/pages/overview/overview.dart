eefb6a199ebc8718ca8ddbdce2f8ecdf591602e1
Yeezy97 <ala.rah@outlook.com>
2022-10-30 17:46:15 +0100

    completed chart

--- lib/pages/overview/overview.dart
+++ lib/pages/overview/overview.dart
@@ -3,14 +3,43 @@ import 'package:flutterwebb/constants/controllers.dart';
 import 'package:flutterwebb/helpers/responsiveness.dart';
 import 'package:flutterwebb/pages/overview/overview_widgets/overview_cards_medium.dart';
 import 'package:flutterwebb/pages/overview/overview_widgets/overview_cards_small.dart';
+import 'package:flutterwebb/widgets/bar_chart.dart';
 import 'package:flutterwebb/widgets/custom_text.dart';
 import 'package:get/get.dart';
+import 'package:charts_flutter_new/flutter.dart' as charts;
 
 import 'overview_widgets/overview_crads_large.dart';
 
 class OverViewPage extends StatelessWidget {
-  const OverViewPage({Key? key}) : super(key: key);
+  OverViewPage({Key? key}) : super(key: key);
 
+  final List<OrdinalSales> data = [
+    OrdinalSales(
+      year: "2017",
+      sales: 10000,
+      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
+    ),
+    OrdinalSales(
+      year: "2018",
+      sales: 12000,
+      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
+    ),
+    OrdinalSales(
+      year: "2019",
+      sales: 9000,
+      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
+    ),
+    OrdinalSales(
+      year: "2020",
+      sales: 15000,
+      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
+    ),
+    OrdinalSales(
+      year: "2021",
+      sales: 18000,
+      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -40,7 +69,8 @@ class OverViewPage extends StatelessWidget {
               else
                 OverViewCardsLarge()
               else
-                OverViewCardsSmall()
+                OverViewCardsSmall(),
+                SalesBarChart(data: data),
             ],
           ),
         ),
--- lib/widgets/bar_chart.dart
+++ lib/widgets/bar_chart.dart
@@ -4,11 +4,38 @@ import 'package:charts_flutter_new/flutter.dart' as charts;
 class SalesBarChart extends StatelessWidget {
   final List<OrdinalSales> data;
 
-  const SalesBarChart({Key? key, required this.data}) : super(key: key);
+  SalesBarChart({Key? key,required this.data}) : super(key: key);
 
 
   @override
   Widget build(BuildContext context) {
+    final List<OrdinalSales> data = [
+      OrdinalSales(
+        year: "2017",
+        sales: 10000,
+        barColor: charts.ColorUtil.fromDartColor(Colors.blue),
+      ),
+      OrdinalSales(
+        year: "2018",
+        sales: 12000,
+        barColor: charts.ColorUtil.fromDartColor(Colors.blue),
+      ),
+      OrdinalSales(
+        year: "2019",
+        sales: 9000,
+        barColor: charts.ColorUtil.fromDartColor(Colors.blue),
+      ),
+      OrdinalSales(
+        year: "2020",
+        sales: 15000,
+        barColor: charts.ColorUtil.fromDartColor(Colors.blue),
+      ),
+      OrdinalSales(
+        year: "2021",
+        sales: 18000,
+        barColor: charts.ColorUtil.fromDartColor(Colors.blue),
+      ),
+    ];
 
     List<charts.Series<OrdinalSales, String>> series = [
       charts.Series(
@@ -22,7 +49,19 @@ class SalesBarChart extends StatelessWidget {
         series.barColor!,
       )
     ];
-    return charts.BarChart(series, animate: true,);
+    return Container(
+      height: 300,
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text("Ordinal Sales by year",
+            style: Theme.of(context).textTheme.bodyText2,),
+            Expanded(child: charts.BarChart(series, animate: true,))
+          ],
+        ),
+      ),
+    );
   }
 }
 
@@ -32,5 +71,6 @@ class OrdinalSales {
   final charts.Color? barColor;
 
   OrdinalSales({this.sales, this.year, this.barColor});
-
 }
+
+
